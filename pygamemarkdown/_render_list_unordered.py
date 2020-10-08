c99363d4afc1739a760ec48387798438eebf3ce1
CribberSix <seifert.victor@web.de>
2020-10-08 19:41:07 +0200

    remove print statements

--- pygamemarkdown/_render_list_unordered.py
+++ pygamemarkdown/_render_list_unordered.py
@@ -6,14 +6,9 @@ def prep_unordered_list(self, textlist: List) -> List:
     res = []
     for line in textlist:
         if re.search(self.pattern_uList_first_indent, line) is not None:
-            print("FIRST-INDENTATION LEVEL")
-            print(line)
             newline = u'\u2022 ' + line[2:]
             res.append(newline)
         elif re.search(self.pattern_uList_second_indent, line) is not None:
-            print("SECOND-INDENTATION LEVEL")
-            print(line)
             newline = u'____\u25E6 ' + line[6:]
             res.append(newline)
-
     return res
